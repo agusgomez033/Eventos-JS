6f22523c032e8d0aa1988d6cb5518a7897d0a702
agusgomez033 <gomezagustin8513@gmail.com>
2022-09-05 13:31:27 -0300

    0.1

---
+++ eventos.js
@@ -0,0 +1,12 @@
+document.getElementById("div")
+document.addEventListener("click", otro_click,true);
+document.getElementById("btn");
+document.addEventListener("click", click,);
+
+function click(){
+    console.log("Accion por el button");
+}
+
+function otro_click(){
+    console.log("Accion por el div")
+}
